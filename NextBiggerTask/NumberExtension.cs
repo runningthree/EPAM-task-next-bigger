ed267edf9aced29f98b55eb5ec8ae5c62a47a2d3
Anzhelika Kravchuk <AnzhelikaKravchuk@users.noreply.github.com>
2020-04-23 20:14:58 +0300

    add-task_and-test-projects

---
+++ NextBiggerTask/NumberExtension.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NextBiggerTask
+{
+    public static class NumberExtension
+    {
+        public static int? NextBiggerThan(int number)
+        {
+            throw new NotImplementedException("You need to implement this function.");
+        }
+    }
+}
