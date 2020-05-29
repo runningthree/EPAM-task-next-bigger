55efe786a77bc9b8f884253625040a0e477962ec
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-05-29 17:14:35 +0000

    Update NumberExtension.cs

--- NextBiggerTask/NumberExtension.cs
+++ NextBiggerTask/NumberExtension.cs
@@ -4,6 +4,16 @@ namespace NextBiggerTask
 {
     public static class NumberExtension
     {
+        /// <summary>
+        /// Takes a positive integer number and returns the nearest largest integer
+        /// consisting of the digits of the original number and null if no such number exists.
+        /// </summary>
+        /// <param name="number">Source number.</param>
+        /// <returns>
+        /// The nearest largest integer consisting of the digits of the original number,
+        /// and null if no such number exists.
+        /// </returns>
+        /// <exception cref="ArgumentException">Thrown if source number less than zero.</exception>
         public static int? NextBiggerThan(int number)
         {
             throw new NotImplementedException("You need to implement this function.");
